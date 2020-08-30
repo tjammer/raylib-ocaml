2e72c312672ed2d9db20396eaf831d41f82e49e3
Max Lantas <mnxn.dev@outlook.com>
2020-08-29 19:35:12 -0700

    link with proper libraries on bsd

--- src/c/config/generate.ml
+++ src/c/config/generate.ml
@@ -15,6 +15,19 @@ let () =
               [ "OpenGL"; "Cocoa"; "IOKit"; "CoreAudio"; "CoreVideo" ]
         | Some ("mingw" | "mingw64" | "win32" | "win64" | "cygwin") ->
             link [ "opengl32"; "gdi32"; "winmm"; "pthread" ]
+        | Some ("netbsd" | "freebsd" | "openbsd" | "bsd" | "bsd_elf") ->
+            link
+              [
+                "GL";
+                "pthread";
+                "m";
+                "X11";
+                "Xrandr";
+                "Xinerama";
+                "Xi";
+                "Xxf86vm";
+                "Xcursor";
+              ]
         | Some system -> C.die "unsupported system: %s" system
         | None -> C.die "unsupported system"
       in
