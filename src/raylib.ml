fb9b486f8fc408f05f4aadd294c0baa6226ba19d
Tobias Jammer <tobiasjammer@googlemail.com>
2020-08-31 14:36:03 +0200

    Open ctypes in raylib.ml

--- src/raylib.ml
+++ src/raylib.ml
@@ -1,65 +1,60 @@
 include Raylib_functions.Description (Raylib_c.Raylib_c_generated_functions)
 include Raylib_generated_constants
-module Types = Raylib_functions.Types
 include Ctypes_reexports
-
-(* TODO
- * open Ctypes *)
+module Types = Raylib_functions.Types
+open Ctypes
 
 module Vector2 = struct
-  type t = Types.Vector2.t Ctypes.structure
+  type t = Types.Vector2.t structure
 
   let t = Types.Vector2.t
 
   let create x y =
-    let open Ctypes in
     let vec = make t in
     setf vec Types.Vector2.x x;
     setf vec Types.Vector2.y y;
     vec
 
-  let x vec = Ctypes.getf vec Types.Vector2.x
+  let x vec = getf vec Types.Vector2.x
 
-  let y vec = Ctypes.getf vec Types.Vector2.y
+  let y vec = getf vec Types.Vector2.y
 
-  let set_x vec x = Ctypes.setf vec Types.Vector2.x x
+  let set_x vec x = setf vec Types.Vector2.x x
 
-  let set_y vec y = Ctypes.setf vec Types.Vector2.y y
+  let set_y vec y = setf vec Types.Vector2.y y
 end
 
 module Vector3 = struct
-  type t = Types.Vector3.t Ctypes.structure
+  type t = Types.Vector3.t structure
 
   let t = Types.Vector3.t
 
   let create x y z =
-    let open Ctypes in
     let vec = make t in
     setf vec Types.Vector3.x x;
     setf vec Types.Vector3.y y;
     setf vec Types.Vector3.z z;
     vec
 
-  let x vec = Ctypes.getf vec Types.Vector3.x
+  let x vec = getf vec Types.Vector3.x
 
-  let y vec = Ctypes.getf vec Types.Vector3.y
+  let y vec = getf vec Types.Vector3.y
 
-  let z vec = Ctypes.getf vec Types.Vector3.z
+  let z vec = getf vec Types.Vector3.z
 
-  let set_x vec x = Ctypes.setf vec Types.Vector3.x x
+  let set_x vec x = setf vec Types.Vector3.x x
 
-  let set_y vec y = Ctypes.setf vec Types.Vector3.y y
+  let set_y vec y = setf vec Types.Vector3.y y
 
-  let set_z vec z = Ctypes.setf vec Types.Vector3.z z
+  let set_z vec z = setf vec Types.Vector3.z z
 end
 
 module Vector4 = struct
-  type t = Types.Vector4.t Ctypes.structure
+  type t = Types.Vector4.t structure
 
   let t = Types.Vector4.t
 
   let create x y z w =
-    let open Ctypes in
     let vec = make t in
     setf vec Types.Vector4.x x;
     setf vec Types.Vector4.y y;
@@ -67,32 +62,31 @@ module Vector4 = struct
     setf vec Types.Vector4.w w;
     vec
 
-  let x vec = Ctypes.getf vec Types.Vector4.x
+  let x vec = getf vec Types.Vector4.x
 
-  let y vec = Ctypes.getf vec Types.Vector4.y
+  let y vec = getf vec Types.Vector4.y
 
-  let z vec = Ctypes.getf vec Types.Vector4.z
+  let z vec = getf vec Types.Vector4.z
 
-  let w vec = Ctypes.getf vec Types.Vector4.w
+  let w vec = getf vec Types.Vector4.w
 
-  let set_x vec x = Ctypes.setf vec Types.Vector4.x x
+  let set_x vec x = setf vec Types.Vector4.x x
 
-  let set_y vec y = Ctypes.setf vec Types.Vector4.y y
+  let set_y vec y = setf vec Types.Vector4.y y
 
-  let set_z vec z = Ctypes.setf vec Types.Vector4.z z
+  let set_z vec z = setf vec Types.Vector4.z z
 
-  let set_w vec w = Ctypes.setf vec Types.Vector4.w w
+  let set_w vec w = setf vec Types.Vector4.w w
 end
 
 module Quaternion = Vector4
 
 module Matrix = struct
-  type t = Types.Matrix.t Ctypes.structure
+  type t = Types.Matrix.t structure
 
   let t = Types.Matrix.t
 
   let create m0 m4 m8 m12 m1 m5 m9 m13 m2 m6 m10 m14 m3 m7 m11 m15 =
-    let open Ctypes in
     let vec = make t in
     setf vec Types.Matrix.m0 m0;
     setf vec Types.Matrix.m1 m1;
@@ -114,12 +108,11 @@ module Matrix = struct
 end
 
 module Color = struct
-  type t = Types.Color.t Ctypes.structure
+  type t = Types.Color.t structure
 
   let t = Types.Color.t
 
   let create r g b a =
-    let open Ctypes in
     let color = make t in
     setf color Types.Color.r (Unsigned.UInt8.of_int r);
     setf color Types.Color.g (Unsigned.UInt8.of_int g);
@@ -182,12 +175,11 @@ module Color = struct
 end
 
 module Rectangle = struct
-  type t = Types.Rectangle.t Ctypes.structure
+  type t = Types.Rectangle.t structure
 
   let t = Types.Rectangle.t
 
   let create x y width height =
-    let open Ctypes in
     let rc = make t in
     setf rc Types.Rectangle.x x;
     setf rc Types.Rectangle.y y;
@@ -195,66 +187,65 @@ module Rectangle = struct
     setf rc Types.Rectangle.height height;
     rc
 
-  let x rc = Ctypes.getf rc Types.Rectangle.x
+  let x rc = getf rc Types.Rectangle.x
 
-  let y rc = Ctypes.getf rc Types.Rectangle.y
+  let y rc = getf rc Types.Rectangle.y
 
-  let width rc = Ctypes.getf rc Types.Rectangle.width
+  let width rc = getf rc Types.Rectangle.width
 
-  let height rc = Ctypes.getf rc Types.Rectangle.height
+  let height rc = getf rc Types.Rectangle.height
 
-  let set_x rc x = Ctypes.setf rc Types.Rectangle.x x
+  let set_x rc x = setf rc Types.Rectangle.x x
 
-  let set_y rc y = Ctypes.setf rc Types.Rectangle.y y
+  let set_y rc y = setf rc Types.Rectangle.y y
 
-  let set_width rc width = Ctypes.setf rc Types.Rectangle.width width
+  let set_width rc width = setf rc Types.Rectangle.width width
 
-  let set_height rc height = Ctypes.setf rc Types.Rectangle.height height
+  let set_height rc height = setf rc Types.Rectangle.height height
 end
 
 module Image = struct
-  type t = Types.Image.t Ctypes.structure
+  type t = Types.Image.t structure
 
-  let width tex = Ctypes.getf tex Types.Texture2D.width
+  let width tex = getf tex Types.Texture2D.width
 
-  let height tex = Ctypes.getf tex Types.Texture2D.height
+  let height tex = getf tex Types.Texture2D.height
 
-  let mipmaps tex = Ctypes.getf tex Types.Texture2D.mipmaps
+  let mipmaps tex = getf tex Types.Texture2D.mipmaps
 
-  let format tex = Ctypes.getf tex Types.Texture2D.format
+  let format tex = getf tex Types.Texture2D.format
 end
 
 module Texture2D = struct
-  type t = Types.Texture2D.t Ctypes.structure
+  type t = Types.Texture2D.t structure
 
-  let id tex = Ctypes.getf tex Types.Texture2D.id
+  let id tex = getf tex Types.Texture2D.id
 
-  let width tex = Ctypes.getf tex Types.Texture2D.width
+  let width tex = getf tex Types.Texture2D.width
 
-  let height tex = Ctypes.getf tex Types.Texture2D.height
+  let height tex = getf tex Types.Texture2D.height
 
-  let mipmaps tex = Ctypes.getf tex Types.Texture2D.mipmaps
+  let mipmaps tex = getf tex Types.Texture2D.mipmaps
 
-  let format tex = Ctypes.getf tex Types.Texture2D.format
+  let format tex = getf tex Types.Texture2D.format
 end
 
 module RenderTexture2D = struct
-  type t = Types.RenderTexture2D.t Ctypes.structure
+  type t = Types.RenderTexture2D.t structure
 
-  let texture tex = Ctypes.getf tex Types.RenderTexture2D.texture
+  let texture tex = getf tex Types.RenderTexture2D.texture
 
-  let depth tex = Ctypes.getf tex Types.RenderTexture2D.depth
+  let depth tex = getf tex Types.RenderTexture2D.depth
 
-  let depth_texture tex = Ctypes.getf tex Types.RenderTexture2D.depth_texture
+  let depth_texture tex = getf tex Types.RenderTexture2D.depth_texture
 end
 
 module NPatchInfo = struct
-  type t = Types.NPatchInfo.t Ctypes.structure
+  type t = Types.NPatchInfo.t structure
 
   let t = Types.NPatchInfo.t
 
   let create rc left top right bottom typ =
-    let open Ctypes in
     let np = make t in
     setf np Types.NPatchInfo.source_rec rc;
     setf np Types.NPatchInfo.left left;
@@ -264,56 +255,56 @@ module NPatchInfo = struct
     setf np Types.NPatchInfo.typ typ;
     np
 
-  let source_rec np = Ctypes.getf np Types.NPatchInfo.source_rec
+  let source_rec np = getf np Types.NPatchInfo.source_rec
 
-  let left np = Ctypes.getf np Types.NPatchInfo.left
+  let left np = getf np Types.NPatchInfo.left
 
-  let top np = Ctypes.getf np Types.NPatchInfo.top
+  let top np = getf np Types.NPatchInfo.top
 
-  let right np = Ctypes.getf np Types.NPatchInfo.right
+  let right np = getf np Types.NPatchInfo.right
 
-  let bottom np = Ctypes.getf np Types.NPatchInfo.bottom
+  let bottom np = getf np Types.NPatchInfo.bottom
 
-  let typ np = Ctypes.getf np Types.NPatchInfo.typ
+  let typ np = getf np Types.NPatchInfo.typ
 end
 
 module CharInfo = struct
-  type t = Types.CharInfo.t Ctypes.structure
+  type t = Types.CharInfo.t structure
 
-  let value char_info = Ctypes.getf char_info Types.CharInfo.value
+  let value char_info = getf char_info Types.CharInfo.value
 
-  let offset_x char_info = Ctypes.getf char_info Types.CharInfo.offset_x
+  let offset_x char_info = getf char_info Types.CharInfo.offset_x
 
-  let offset_y char_info = Ctypes.getf char_info Types.CharInfo.offset_y
+  let offset_y char_info = getf char_info Types.CharInfo.offset_y
 
-  let advance_x char_info = Ctypes.getf char_info Types.CharInfo.advance_x
+  let advance_x char_info = getf char_info Types.CharInfo.advance_x
 
-  let image char_info = Ctypes.getf char_info Types.CharInfo.image
+  let image char_info = getf char_info Types.CharInfo.image
 end
 
 module Font = struct
-  type t = Types.Font.t Ctypes.structure
+  type t = Types.Font.t structure
 
-  let base_size font = Ctypes.getf font Types.Font.base_size
+  let base_size font = getf font Types.Font.base_size
 
-  let chars_count font = Ctypes.getf font Types.Font.chars_count
+  let chars_count font = getf font Types.Font.chars_count
 
-  let texture font = Ctypes.getf font Types.Font.texture
+  let texture font = getf font Types.Font.texture
 
-  let recs font = Ctypes.getf font Types.Font.recs
+  let recs font = getf font Types.Font.recs
 
-  let chars font = Ctypes.getf font Types.Font.base_size
+  let chars font =
+    CArray.from_ptr (getf font Types.Font.chars) (chars_count font)
 end
 
 module CameraType = Types.CameraType
 
 module Camera3D = struct
-  type t = Types.Camera3D.t Ctypes.structure
+  type t = Types.Camera3D.t structure
 
   let t = Types.Camera3D.t
 
   let create position target up fovy typ =
-    let open Ctypes in
     let cam = make t in
     setf cam Types.Camera3D.position position;
     setf cam Types.Camera3D.target target;
@@ -322,26 +313,25 @@ module Camera3D = struct
     setf cam Types.Camera3D.typ typ;
     cam
 
-  let position cam = Ctypes.getf cam Types.Camera3D.position
+  let position cam = getf cam Types.Camera3D.position
 
-  let target cam = Ctypes.getf cam Types.Camera3D.target
+  let target cam = getf cam Types.Camera3D.target
 
-  let up cam = Ctypes.getf cam Types.Camera3D.up
+  let up cam = getf cam Types.Camera3D.up
 
-  let fovy cam = Ctypes.getf cam Types.Camera3D.fovy
+  let fovy cam = getf cam Types.Camera3D.fovy
 
-  let typ cam = Ctypes.getf cam Types.Camera3D.typ
+  let typ cam = getf cam Types.Camera3D.typ
 end
 
 module Camera = Camera3D
 
 module Camera2D = struct
-  type t = Types.Camera2D.t Ctypes.structure
+  type t = Types.Camera2D.t structure
 
   let t = Types.Camera2D.t
 
   let create offset target rotation zoom =
-    let open Ctypes in
     let cam = make t in
     setf cam Types.Camera2D.offset offset;
     setf cam Types.Camera2D.target target;
@@ -349,19 +339,19 @@ module Camera2D = struct
     setf cam Types.Camera2D.zoom zoom;
     cam
 
-  let offset cam = Ctypes.getf cam Types.Camera2D.offset
+  let offset cam = getf cam Types.Camera2D.offset
 
-  let target cam = Ctypes.getf cam Types.Camera2D.target
+  let target cam = getf cam Types.Camera2D.target
 
-  let rotation cam = Ctypes.getf cam Types.Camera2D.rotation
+  let rotation cam = getf cam Types.Camera2D.rotation
 
-  let zoom cam = Ctypes.getf cam Types.Camera2D.zoom
+  let zoom cam = getf cam Types.Camera2D.zoom
 end
 
 module Mesh = Types.Mesh
 
 module Shader = struct
-  type t = Types.Shader.t Ctypes.structure
+  type t = Types.Shader.t structure
 
   let t = Types.Shader.t
 end
@@ -369,151 +359,133 @@ end
 module MaterialMapType = Types.MaterialMapType
 
 module MaterialMap = struct
-  type t = Types.MaterialMap.t Ctypes.structure
+  type t = Types.MaterialMap.t structure
 
   let t = Types.MaterialMap.t
 
   let create texture color value =
-    let open Ctypes in
     let map = make t in
     setf map Types.MaterialMap.texture texture;
     setf map Types.MaterialMap.color color;
     setf map Types.MaterialMap.value value;
     map
 
-  let texture map = Ctypes.getf map Types.MaterialMap.texture
+  let texture map = getf map Types.MaterialMap.texture
 
-  let color map = Ctypes.getf map Types.MaterialMap.color
+  let color map = getf map Types.MaterialMap.color
 
-  let value map = Ctypes.getf map Types.MaterialMap.value
+  let value map = getf map Types.MaterialMap.value
 
-  let set_texture map texture =
-    Ctypes.setf map Types.MaterialMap.texture texture
+  let set_texture map texture = setf map Types.MaterialMap.texture texture
 
-  let set_color map color = Ctypes.setf map Types.MaterialMap.color color
+  let set_color map color = setf map Types.MaterialMap.color color
 
-  let set_value map value = Ctypes.setf map Types.MaterialMap.value value
+  let set_value map value = setf map Types.MaterialMap.value value
 end
 
 module Material = struct
-  type t = Types.Material.t Ctypes.structure
+  type t = Types.Material.t structure
 
   let t = Types.Material.t
 
   let create shader maps params =
-    let open Ctypes in
     let material = make t in
     setf material Types.Material.shader shader;
     setf material Types.Material.maps maps;
     setf material Types.Material.params params;
     material
 
-  let shader material = Ctypes.getf material Types.Material.shader
+  let shader material = getf material Types.Material.shader
 
   let maps material =
-    Ctypes.CArray.from_ptr
-      (Ctypes.getf material Types.Material.maps)
-      max_material_maps
+    CArray.from_ptr (getf material Types.Material.maps) max_material_maps
 
   let params material =
-    Ctypes.CArray.from_ptr
-      (Ctypes.getf material Types.Material.params)
-      max_shader_locations
+    CArray.from_ptr (getf material Types.Material.params) max_shader_locations
 
-  let set_shader material shader =
-    Ctypes.setf material Types.Material.shader shader
+  let set_shader material shader = setf material Types.Material.shader shader
 
   let set_maps material maps =
-    Ctypes.setf material Types.Material.maps (Ctypes.CArray.start maps)
+    setf material Types.Material.maps (CArray.start maps)
 end
 
 module Transform = struct
-  type t = Types.Transform.t Ctypes.structure
+  type t = Types.Transform.t structure
 
   let t = Types.Transform.t
 
   let create translation rotation scale =
-    let open Ctypes in
     let transform = make t in
     setf transform Types.Transform.translation translation;
     setf transform Types.Transform.rotation rotation;
     setf transform Types.Transform.scale scale;
     transform
 
-  let translation transform = Ctypes.getf transform Types.Transform.translation
+  let translation transform = getf transform Types.Transform.translation
 
-  let rotation transform = Ctypes.getf transform Types.Transform.rotation
+  let rotation transform = getf transform Types.Transform.rotation
 
-  let scale transform = Ctypes.getf transform Types.Transform.scale
+  let scale transform = getf transform Types.Transform.scale
 
   let set_translation transform translation =
-    Ctypes.setf transform Types.Transform.translation translation
+    setf transform Types.Transform.translation translation
 
   let set_rotation transform rotation =
-    Ctypes.setf transform Types.Transform.rotation rotation
+    setf transform Types.Transform.rotation rotation
 
-  let set_scale transform scale =
-    Ctypes.setf transform Types.Transform.scale scale
+  let set_scale transform scale = setf transform Types.Transform.scale scale
 end
 
 module BoneInfo = Types.BoneInfo
 
 module Model = struct
-  type t = Types.Model.t Ctypes.structure
+  type t = Types.Model.t structure
 
   let t = Types.Model.t
 
-  let transform model = Ctypes.getf model Types.Model.transform
+  let transform model = getf model Types.Model.transform
 
   let meshes model =
-    let open Ctypes in
     let count = getf model Types.Model.mesh_count in
     CArray.from_ptr (getf model Types.Model.meshes) count
 
   let materials model =
-    let open Ctypes in
     let count = getf model Types.Model.material_count in
     CArray.from_ptr (getf model Types.Model.materials) count
 
   let bones model =
-    let open Ctypes in
     let count = getf model Types.Model.bone_count in
     CArray.from_ptr (getf model Types.Model.bones) count
 
-  let bind_pose model = Ctypes.getf model Types.Model.bind_pose
+  let bind_pose model = getf model Types.Model.bind_pose
 
-  let set_transform model transform =
-    Ctypes.setf model Types.Model.transform transform
+  let set_transform model transform = setf model Types.Model.transform transform
 
   let set_meshes model meshes =
-    Ctypes.setf model Types.Model.meshes (Ctypes.CArray.start meshes)
+    setf model Types.Model.meshes (CArray.start meshes)
 
   let set_materials model materials =
-    Ctypes.setf model Types.Model.materials (Ctypes.CArray.start materials)
+    setf model Types.Model.materials (CArray.start materials)
 
-  let set_bones model bones =
-    Ctypes.setf model Types.Model.bones (Ctypes.CArray.start bones)
+  let set_bones model bones = setf model Types.Model.bones (CArray.start bones)
 
-  let set_bind_pose model bind_pose =
-    Ctypes.setf model Types.Model.bind_pose bind_pose
+  let set_bind_pose model bind_pose = setf model Types.Model.bind_pose bind_pose
 end
 
 module ModelAnimation = struct
-  type t = Types.ModelAnimation.t Ctypes.structure
+  type t = Types.ModelAnimation.t structure
 
   let t = Types.Ray.t
 
   let bones anim =
-    let open Ctypes in
     let count = getf anim Types.ModelAnimation.bone_count in
     CArray.from_ptr (getf anim Types.ModelAnimation.bones) count
 
-  let bone_count anim = Ctypes.getf anim Types.ModelAnimation.bone_count
+  let bone_count anim = getf anim Types.ModelAnimation.bone_count
 
-  let frame_count anim = Ctypes.getf anim Types.ModelAnimation.frame_count
+  let frame_count anim = getf anim Types.ModelAnimation.frame_count
 
   let frame_pose_at anim index =
-    let open Ctypes in
     let poses =
       CArray.from_ptr
         (getf anim Types.ModelAnimation.frame_poses)
@@ -523,32 +495,31 @@ module ModelAnimation = struct
 end
 
 module Ray = struct
-  type t = Types.Ray.t Ctypes.structure
+  type t = Types.Ray.t structure
 
   let t = Types.Ray.t
 
   let create position direction =
-    let open Ctypes in
     let ray = make t in
     setf ray Types.Ray.position position;
     setf ray Types.Ray.direction direction;
     ray
 
-  let position ray = Ctypes.getf ray Types.Ray.position
+  let position ray = getf ray Types.Ray.position
 
-  let direction ray = Ctypes.getf ray Types.Ray.direction
+  let direction ray = getf ray Types.Ray.direction
 end
 
 module RayHitInfo = struct
-  type t = Types.RayHitInfo.t Ctypes.structure
+  type t = Types.RayHitInfo.t structure
 
-  let hit ray = Ctypes.getf ray Types.RayHitInfo.hit
+  let hit ray = getf ray Types.RayHitInfo.hit
 
-  let distance ray = Ctypes.getf ray Types.RayHitInfo.distance
+  let distance ray = getf ray Types.RayHitInfo.distance
 
-  let position ray = Ctypes.getf ray Types.RayHitInfo.position
+  let position ray = getf ray Types.RayHitInfo.position
 
-  let normal ray = Ctypes.getf ray Types.RayHitInfo.normal
+  let normal ray = getf ray Types.RayHitInfo.normal
 end
 
 (* TODO *)
@@ -562,4 +533,4 @@ module ConfigFlag = Types.ConfigFlag
 let load_model_animations path =
   let count = ptr_of_int 0 in
   let anims = _load_model_animations path count in
-  CArray.from_ptr anims (Ctypes.( !@ ) count)
+  CArray.from_ptr anims !@count
