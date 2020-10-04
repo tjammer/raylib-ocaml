f556ec5957d6e7f88a6e31545e6528d5674518b0
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2020-10-04 23:36:04 +0200

    fix evening after landing, implement push bounds and refactorize things

--- examples/core/core_2d_camera_platformer.ml
+++ examples/core/core_2d_camera_platformer.ml
@@ -9,7 +9,7 @@ type camera_mode_t = Center
                    | EvenOutOnLanding of bool * float
                    | PlayerBoundsPush
 
-type state_t = player_t * env_item_t Array.t * Raylib.Camera2D.t * camera_mode_t
+type state_t = player_t * env_item_t List.t * Raylib.Camera2D.t * camera_mode_t
 
 (* Constants *)
 let width = 800
@@ -25,16 +25,22 @@ let player_jump_speed = 350.
 
 let gravity = 400.
 
+let smooth_min_speed = 30.0
+let smooth_min_length = 10.0
+let smooth_fraction = 0.8
+
 let evening_speed = 700.0
+
+let bbox_x, bbox_y = 0.2, 0.2
 
 (* Helpers *)
 
 type comparision_t = Higher | Lower | Equal
 
-let compare a b =
-  if a > b then Higher
-  else if a < b then Lower
-  else Equal
+let compare tol a b =
+  if Float.abs (a -. b) < tol then Equal
+  else if a > b then Higher
+  else Lower
 
 let player_rec player =
   let open Raylib in
@@ -66,13 +72,13 @@ let setup () =
   set_config_flags [ ConfigFlag.Window_resizable ];
   init_window width height "raylib [core] example - 2d camera";
   let player = {position=half_screen; speed=0.0; can_jump=false} in
-  let env_items = [|
+  let env_items = [
     {box=(Rectangle.create 0.0 0.0 1000.0 500.0); blocking=false; color=Color.lightgray };
     {box=(Rectangle.create 0.0 400.0 1000.0 200.0); blocking=true; color=Color.gray };
     {box=(Rectangle.create 300.0 200.0 400.0 10.0); blocking=true; color=Color.gray };
     {box=(Rectangle.create 250.0 300.0 100.0 10.0); blocking=true; color=Color.gray };
     {box=(Rectangle.create 650.0 300.0 100.0 10.0); blocking=true; color=Color.gray }
-  |]
+  ]
   in
   let camera =
     Camera2D.create
@@ -85,9 +91,7 @@ let setup () =
 
 let update_player delta ({position; speed; can_jump}, env_items, c, m) =
   let open Raylib in
-  let x, y =
-    (Vector2.x position, Vector2.y position)
-  in
+  let x, y = (Vector2.x position, Vector2.y position) in
   let x =
     match is_key_down Key.Left, is_key_down Key.Right with
     | true, false -> x -. (player_hor_speed *. delta)
@@ -99,21 +103,24 @@ let update_player delta ({position; speed; can_jump}, env_items, c, m) =
     then ~-.player_jump_speed
     else speed
   in
-  let player_collide_with elem =
-    elem.blocking
-    && (Rectangle.x elem.box) <= x
-    && (Rectangle.x elem.box) +. (Rectangle.width elem.box) >= x
-    && (Rectangle.y elem.box) >= y
-    && (Rectangle.y elem.box) <= y +. speed *. delta
-  in
   let player =
     if is_key_pressed Key.R
     then {position=half_screen; speed=0.0; can_jump=true}
-    else if Array.exists player_collide_with env_items
-    then {position=(Vector2.create x y); speed=0.; can_jump=true}
-    else {position=(Vector2.create x (y +. (speed *.  delta)));
-          speed=speed +. gravity *. delta;
-          can_jump=false}
+    else let rec player_collide_with = function
+        | [] -> None
+        | {blocking=true; box; _} :: _ when
+            (Rectangle.x box) <= x
+            && (Rectangle.x box) +. (Rectangle.width box) >= x
+            && (Rectangle.y box) >= y
+            && (Rectangle.y box) <= y +. speed *. delta -> Some (Rectangle.y box)
+        | _ :: rest -> player_collide_with rest
+      in
+      match player_collide_with env_items with
+      | Some y -> {position=(Vector2.create x y); speed=0.; can_jump=true}
+      | None ->
+        {position=(Vector2.create x (y +. (speed *.  delta)));
+         speed=speed +. gravity *. delta;
+         can_jump=false}
   in
   (player, env_items, c, m)
 
@@ -133,64 +140,55 @@ let update_camera delta (player, env_items, camera, mode) =
     match mode with
     | Center -> (half_screen, player.position, mode)
     | CenterInsideMap ->
-      let (minX, minY, maxX, maxY) =
-        Array.fold_right
-          (fun elem (minX, minY, maxX, maxY) ->
-             Float.min minX (Rectangle.x elem.box),
-             Float.min minY (Rectangle.y elem.box),
-             Float.max
-               maxX
-               ((Rectangle.x elem.box) +. (Rectangle.width elem.box)),
-             Float.max
-               maxY
-               ((Rectangle.y elem.box) +. (Rectangle.height elem.box)))
-          env_items
-          (1000.0, 1000.0, ~-.1000.0 , ~-.1000.0)
-      in
-      let vmin = get_world_to_screen_2d (Vector2.create minX minY) camera in
-      let vmax = get_world_to_screen_2d (Vector2.create maxX maxY) camera in
-      let clamp_cam get_coord maxi =
-        let low, high = (get_coord vmin, get_coord vmax) in
-        if low > 0.0 then ~-.low
-        else if high < maxi then maxi -. high
-        else 0.0
-      in
       let shift =
+        let (minX, minY, maxX, maxY) =
+          List.fold_right
+            (fun elem (minX, minY, maxX, maxY) ->
+               let x, y, w, h =
+                 Rectangle.x elem.box, Rectangle.y elem.box,
+                 Rectangle.width elem.box, Rectangle.height elem.box
+               in
+               (Float.min minX x, Float.min minY y,
+                Float.max maxX (x +. w), Float.max maxY (y +. h)))
+            env_items
+            (1000.0, 1000.0, ~-.1000.0 , ~-.1000.0)
+        in
+        let vmin = get_world_to_screen_2d (Vector2.create minX minY) camera in
+        let vmax = get_world_to_screen_2d (Vector2.create maxX maxY) camera in
+        let shift_cam get_coord maxi =
+          let low, high = (get_coord vmin, get_coord vmax) in
+          if low > 0.0 then ~-.low
+          else if high < maxi then maxi -. high
+          else 0.0
+        in
         Vector2.create
-          (clamp_cam Vector2.x (Float.of_int width))
-          (clamp_cam Vector2.y (Float.of_int height))
+          (shift_cam Vector2.x (Float.of_int width))
+          (shift_cam Vector2.y (Float.of_int height))
       in
-      let offset =
-        if Vector2.length shift > 0.1
-        then Vector2.add half_screen shift
-        else half_screen
-      in
+      let offset = Vector2.add half_screen shift in
       (* This is just very buggy unfortunalty *)
-      ignore (offset, player.position, mode);
-      (half_screen, player.position, mode)
+      (offset, player.position, mode)
+    (* (half_screen, player.position, mode) *)
     | SmoothFollow ->
       let target =
-        let min_speed = 30.0 in
-        let min_effect_length = 10.0 in
-        let fraction_speed = 0.8 in
-        let diff =
-          Vector2.subtract player.position (Camera2D.target camera)
-        in
+        let diff = Vector2.subtract player.position (Camera2D.target camera) in
         let length = Vector2.length diff in
-        if length < min_effect_length
-        then (Camera2D.target camera)
-        else let speed =
-               Float.max (fraction_speed *. length) min_speed
-          in
+        if length < smooth_min_length
+        then Camera2D.target camera
+        else
           Vector2.add
             (Camera2D.target camera)
-            (Vector2.scale diff (speed *. delta /. length))
+            (Vector2.scale
+               diff 
+               (delta 
+                *. Float.max smooth_fraction (smooth_min_speed /. length)))
       in
       (half_screen, target, mode)
     | EvenOutOnLanding (true, evening_target) ->
       let target_y = Vector2.y (Camera2D.target camera) in
       let target_y, evening_out, evening_target =
-        match compare evening_target target_y with
+        match compare 1.0 evening_target target_y with
+        | Equal -> (evening_target, false, 0.0)
         | Higher ->
           let target_y = target_y +. evening_speed *.  delta in
           if evening_target <= target_y
@@ -203,31 +201,51 @@ let update_camera delta (player, env_items, camera, mode) =
           if evening_target >= target_y
           then (evening_target, false, 0.0)
           else (target_y, true, evening_target)
-        | Equal -> (target_y, false, 0.0)
       in
       (half_screen,
-      Vector2.create (Vector2.x player.position) target_y,
-      EvenOutOnLanding (evening_out, evening_target))
+       Vector2.create (Vector2.x player.position) target_y,
+       EvenOutOnLanding (evening_out, evening_target))
     | EvenOutOnLanding (false, _) ->
-      let target = Camera2D.target camera in
+      let target_y = Vector2.y (Camera2D.target camera) in
       let start_moving =
         player.can_jump
         && player.speed == 0.0
-        && (Vector2.y player.position) != Vector2.y target
+        && (Vector2.y player.position) != target_y
       in
       (half_screen,
-       target,
-       EvenOutOnLanding (
-         start_moving,
-         if start_moving then Vector2.y player.position else 0.0))
+       Vector2.create (Vector2.x player.position) target_y,
+       EvenOutOnLanding 
+         (start_moving,
+          if start_moving then Vector2.y player.position else 0.0))
 
-    | PlayerBoundsPush -> (half_screen, player.position, mode)
+    | PlayerBoundsPush ->
+      let vmin = 
+        Vector2.create
+          ((1.0 -. bbox_x) *. 0.5 *. Float.of_int width)
+          ((1.0 -.  bbox_y) *. 0.5 *. Float.of_int height)
+      in
+      let bbox_world_min, bbox_world_max = 
+        get_screen_to_world_2d vmin camera,
+        get_screen_to_world_2d
+          (Vector2.create ((1.0 +. bbox_x) *. 0.5 *. Float.of_int width)
+             ((1.0 +.  bbox_y) *. 0.5 *. Float.of_int height))
+          camera
+      in
+      let clamp_cam get_coord =
+        if get_coord player.position > get_coord bbox_world_max
+        then get_coord bbox_world_min +. get_coord player.position
+             -. get_coord bbox_world_max
+        else if get_coord player.position < get_coord bbox_world_min
+        then get_coord player.position
+        else get_coord (Camera2D.target camera)
+      in
+      let target_x, target_y = clamp_cam Vector2.x, clamp_cam Vector2.y in
+      (vmin, Vector2.create target_x target_y, mode)
   in
   let zoom =
     if is_key_pressed Key.R then 1.0
     else clamp_zoom
-        (Camera2D.zoom camera
-         +. 0.05 *. Float.of_int (get_mouse_wheel_move ()))
+        (Camera2D.zoom camera +. 0.05 *. Float.of_int (get_mouse_wheel_move ()))
   in
   (player,
    env_items,
@@ -246,7 +264,7 @@ let draw_all (player, env_items, camera, mode) =
 
     begin_mode_2d camera;
     (* Draw environment *)
-    Array.iter
+    List.iter
       (fun item -> draw_rectangle_rec item.box item.color)
       env_items;
     (* Draw player *)
