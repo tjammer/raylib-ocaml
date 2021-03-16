1a3b66bdf3fcf62c6c5247fbecdf578f1ecf54d3
Tobias Mock <tobiasjammer@googlemail.com>
2021-03-16 22:50:52 +0100

    WIP work on example

--- examples/gui/controls_test_suite.ml
+++ examples/gui/controls_test_suite.ml
@@ -6,23 +6,32 @@ type state = {
   force_squared_check : bool;
   spinner1_val : int;
   spinner_edit_mode : bool;
+  value_box_val : int;
+  value_box_edit_mode : bool;
+  text_box_text : string;
+  text_box_edit_mode : bool;
+  show_text_input_box : bool;
+  combo_box_active : int;
+  dropdown1_active : int;
+  dropdown1_edit_mode : bool;
 }
 
 let setup () =
   Raylib.init_window width height "raygui - controls test suite";
-  { force_squared_check = false; spinner1_val = 0; spinner_edit_mode = false }
-
-module Rect = struct
-  let t = Raylib_generated_types.Rectangle.t
-
-  let create x y width height =
-    let rc = Ctypes.make t in
-    Ctypes.setf rc Raylib_generated_types.Rectangle.x x;
-    Ctypes.setf rc Raylib_generated_types.Rectangle.y y;
-    Ctypes.setf rc Raylib_generated_types.Rectangle.width width;
-    Ctypes.setf rc Raylib_generated_types.Rectangle.height height;
-    rc
-end
+  Raylib.set_target_fps 60;
+  {
+    force_squared_check = false;
+    spinner1_val = 0;
+    spinner_edit_mode = false;
+    value_box_edit_mode = false;
+    value_box_val = 50;
+    text_box_text = "Text box";
+    text_box_edit_mode = false;
+    show_text_input_box = false;
+    combo_box_active = 0;
+    dropdown1_active = 0;
+    dropdown1_edit_mode = false;
+  }
 
 let rec loop s =
   match Raylib.window_should_close () with
@@ -35,18 +44,98 @@ let rec loop s =
       begin_drawing ();
       clear_background col;
 
+      let rect = Rectangle.create 25.0 108.0 15.0 15.0 in
       let force_squared_check =
-        Raygui.(
-          check_box
-            (Rect.create 25.0 108.0 15.0 15.0)
-            "FORCE CHECK!" s.force_squared_check)
+        Raygui.(check_box rect "FORCE CHECK!" s.force_squared_check)
       in
 
       Raygui.(set_style (Textbox `Text_alignment) TextAlignment.(to_int Center));
 
-      draw_text "Congrats! You created your first window!" 190 200 20
-        Color.lightgray;
+      let rect = Rectangle.create 25.0 135.0 125.0 30.0 in
+      let spinner1_val, spinner_edit_mode =
+        match
+          Raygui.spinner rect "" s.spinner1_val ~min:0 ~max:100
+            s.spinner_edit_mode
+        with
+        | vl, true -> (vl, not s.spinner_edit_mode)
+        | vl, false -> (vl, s.spinner_edit_mode)
+      in
+
+      let rect = Rectangle.create 25.0 175.0 125.0 30.0 in
+      let value_box_val, value_box_edit_mode =
+        match
+          Raygui.value_box rect "" s.value_box_val ~min:0 ~max:100
+            s.value_box_edit_mode
+        with
+        | vl, true -> (vl, not s.value_box_edit_mode)
+        | vl, false -> (vl, s.value_box_edit_mode)
+      in
+
+      Raygui.(set_style (Textbox `Text_alignment) TextAlignment.(to_int Left));
+
+      let rect = Rectangle.create 25.0 215.0 125.0 30.0 in
+      let text_box_text, text_box_edit_mode =
+        match Raygui.text_box rect s.text_box_text s.text_box_edit_mode with
+        | vl, true -> (vl, not s.text_box_edit_mode)
+        | vl, false -> (vl, s.text_box_edit_mode)
+      in
+
+      Raygui.(set_style (Textbox `Text_alignment) TextAlignment.(to_int Center));
+
+      Raygui.set_tooltip "Save current file.";
+      let rect = Rectangle.create 25.0 255.0 125.0 30.0 in
+      let show_text_input_box =
+        if Raygui.button rect "Save File" then true else s.show_text_input_box
+      in
+      Raygui.clear_tooltip ();
+
+      Raygui.group_box (Rectangle.create 25.0 310.0 125.0 150.0) "STATES";
+      Raygui.lock ();
+      Raygui.(
+        set_state ControlState.Normal;
+        ignore (button (Rectangle.create 30.0 320.0 115.0 30.0) "NORMAL"));
+      Raygui.(
+        set_state ControlState.Focused;
+        ignore (button (Rectangle.create 30.0 355.0 115.0 30.0) "FOCUSED"));
+      Raygui.(
+        set_state ControlState.Pressed;
+        ignore (button (Rectangle.create 30.0 390.0 115.0 30.0) "#15#PRESSED"));
+      Raygui.(
+        set_state ControlState.Disabled;
+        ignore (button (Rectangle.create 30.0 425.0 115.0 30.0) "DISABLED"));
+      Raygui.set_state Raygui.ControlState.Normal;
+      Raygui.unlock ();
+
+      let rect = Rectangle.create 25.0 470.0 125.0 30.0 in
+      let combo_box_active =
+        Raygui.combo_box rect "ONE;TWO;THREE;FOUR" s.combo_box_active
+      in
+
+      (* NOTE: GuiDropdownBox must draw after any other control that can be covered on unfolding *)
+      let rect = Rectangle.create 25.0 65.0 125.0 30.0 in
+      let dropdown1_active, dropdown1_edit_mode =
+        match
+          Raygui.dropdown_box rect "ONE;TWO;THREE;FOUR" s.dropdown1_active
+            s.dropdown1_edit_mode
+        with
+        | vl, true -> (vl, not s.dropdown1_edit_mode)
+        | vl, false -> (vl, s.dropdown1_edit_mode)
+      in
+
       end_drawing ();
-      loop { s with force_squared_check }
+      loop
+        {
+          force_squared_check;
+          spinner_edit_mode;
+          spinner1_val;
+          value_box_val;
+          value_box_edit_mode;
+          text_box_text;
+          text_box_edit_mode;
+          show_text_input_box;
+          combo_box_active;
+          dropdown1_active;
+          dropdown1_edit_mode;
+        }
 
 let () = setup () |> loop
--- src/c/functions/raygui_functions.ml
+++ src/c/functions/raygui_functions.ml
@@ -22,26 +22,27 @@ module Description (F : Ctypes.FOREIGN) = struct
   let fade = foreign "GuiFade" (float @-> returning void)
 
   (*  Set gui state (global state) *)
-  let set_state = foreign "GuiSetState" (int @-> returning void)
+  let set_state = foreign "GuiSetState" (Constants.ControlState.t @-> returning void)
 
   (*  Get gui state (global state) *)
-  let get_state = foreign "GuiGetState" (void @-> returning int)
+  let get_state = foreign "GuiGetState" (void @-> returning Constants.ControlState.t)
 
   (* Font set/get functions *)
   (*  Set gui custom font (global state) *)
-  let set_font = foreign "GuiSetFont" (Types.Font.t @-> returning void)
+  let _set_font = foreign "GuiSetFont" (Types.Font.t @-> returning void)
 
   (*  Get gui custom font (global state) *)
-  let get_font = foreign "GuiGetFont" (void @-> returning Types.Font.t)
+  let _get_font = foreign "GuiGetFont" (void @-> returning Types.Font.t)
 
   (* Style set/get functions *)
   (*  Set one style property *)
-  let _set_style = foreign "GuiSetStyle" (Constants.Wrapped_Control.t @-> int @-> int @-> returning void)
+  let _set_style =
+    foreign "GuiSetStyle"
+      (Constants.Wrapped_Control.t @-> int @-> int @-> returning void)
 
   (*  Get one style property *)
   let _get_style =
-    foreign "GuiGetStyle"
-      (Constants.Wrapped_Control.t @-> int @-> returning int)
+    foreign "GuiGetStyle" (Constants.Wrapped_Control.t @-> int @-> returning int)
 
   (* Tooltips set functions *)
   (*  Enable gui tooltips *)
@@ -58,171 +59,171 @@ module Description (F : Ctypes.FOREIGN) = struct
 
   (* Container/separator controls, useful for controls organization *)
   (*  Window Box control, shows a window that can be closed *)
-  let window_box =
+  let _window_box =
     foreign "GuiWindowBox" (Types.Rectangle.t @-> string @-> returning bool)
 
   (*  Group Box control with text name *)
-  let group_box =
+  let _group_box =
     foreign "GuiGroupBox" (Types.Rectangle.t @-> string @-> returning void)
 
   (*  Line separator control, could contain text *)
-  let line = foreign "GuiLine" (Types.Rectangle.t @-> string @-> returning void)
+  let _line = foreign "GuiLine" (Types.Rectangle.t @-> string @-> returning void)
 
   (*  Panel control, useful to group controls *)
-  let panel = foreign "GuiPanel" (Types.Rectangle.t @-> returning void)
+  let _panel = foreign "GuiPanel" (Types.Rectangle.t @-> returning void)
 
   (*  Scroll Panel control *)
-  let scroll_panel =
+  let _scroll_panel =
     foreign "GuiScrollPanel"
       (Types.Rectangle.t @-> Types.Rectangle.t @-> ptr Types.Vector2.t
       @-> returning Types.Rectangle.t)
 
   (* Basic controls set *)
   (*  Label control, shows text *)
-  let label =
+  let _label =
     foreign "GuiLabel" (Types.Rectangle.t @-> string @-> returning void)
 
   (*  Button control, returns true when clicked *)
-  let button =
+  let _button =
     foreign "GuiButton" (Types.Rectangle.t @-> string @-> returning bool)
 
   (*  Label button control, show true when clicked *)
-  let label_button =
+  let _label_button =
     foreign "GuiLabelButton" (Types.Rectangle.t @-> string @-> returning bool)
 
   (*  Image button control, returns true when clicked *)
-  let image_button =
+  let _image_button =
     foreign "GuiImageButton"
       (Types.Rectangle.t @-> string @-> Types.Texture.t @-> returning bool)
 
   (*  Image button extended control, returns true when clicked *)
-  let image_button_ex =
+  let _image_button_ex =
     foreign "GuiImageButtonEx"
       (Types.Rectangle.t @-> string @-> Types.Texture.t @-> Types.Rectangle.t
      @-> returning bool)
 
   (*  Toggle Button control, returns true when active *)
-  let toggle =
+  let _toggle =
     foreign "GuiToggle"
       (Types.Rectangle.t @-> string @-> bool @-> returning bool)
 
   (*  Toggle Group control, returns active toggle index *)
-  let toggle_group =
+  let _toggle_group =
     foreign "GuiToggleGroup"
       (Types.Rectangle.t @-> string @-> int @-> returning int)
 
   (*  Check Box control, returns true when active *)
-  let check_box =
+  let _check_box =
     foreign "GuiCheckBox"
       (Types.Rectangle.t @-> string @-> bool @-> returning bool)
 
   (*  Combo Box control, returns selected item index *)
-  let combo_box =
+  let _combo_box =
     foreign "GuiComboBox"
       (Types.Rectangle.t @-> string @-> int @-> returning int)
 
   (*  Dropdown Box control, returns selected item *)
-  let dropdown_box =
+  let _dropdown_box =
     foreign "GuiDropdownBox"
       (Types.Rectangle.t @-> string @-> ptr int @-> bool @-> returning bool)
 
   (*  Spinner control, returns selected value *)
-  let spinner =
+  let _spinner =
     foreign "GuiSpinner"
       (Types.Rectangle.t @-> string @-> ptr int @-> int @-> int @-> bool
      @-> returning bool)
 
   (*  Value Box control, updates input text with numbers *)
-  let value_box =
+  let _value_box =
     foreign "GuiValueBox"
       (Types.Rectangle.t @-> string @-> ptr int @-> int @-> int @-> bool
      @-> returning bool)
 
   (*  Text Box control, updates input text *)
-  let text_box =
+  let _text_box =
     foreign "GuiTextBox"
-      (Types.Rectangle.t @-> string @-> int @-> bool @-> returning bool)
+      (Types.Rectangle.t @-> ptr char @-> int @-> bool @-> returning bool)
 
   (*  Text Box control with multiple lines *)
-  let text_box_multi =
+  let _text_box_multi =
     foreign "GuiTextBoxMulti"
-      (Types.Rectangle.t @-> char @-> int @-> bool @-> returning bool)
+      (Types.Rectangle.t @-> ptr string @-> int @-> bool @-> returning bool)
 
   (*  Slider control, returns selected value *)
-  let slider =
+  let _slider =
     foreign "GuiSlider"
       (Types.Rectangle.t @-> string @-> string @-> float @-> float @-> float
      @-> returning float)
 
   (*  Slider Bar control, returns selected value *)
-  let slider_bar =
+  let _slider_bar =
     foreign "GuiSliderBar"
       (Types.Rectangle.t @-> string @-> string @-> float @-> float @-> float
      @-> returning float)
 
   (*  Progress Bar control, shows current progress value *)
-  let progress_bar =
+  let _progress_bar =
     foreign "GuiProgressBar"
       (Types.Rectangle.t @-> string @-> string @-> float @-> float @-> float
      @-> returning float)
 
   (*  Status Bar control, shows info text *)
-  let status_bar =
+  let _status_bar =
     foreign "GuiStatusBar" (Types.Rectangle.t @-> string @-> returning void)
 
   (*  Dummy control for placeholders *)
-  let dummy_rec =
+  let _dummy_rec =
     foreign "GuiDummyRec" (Types.Rectangle.t @-> string @-> returning void)
 
   (*  Scroll Bar control *)
-  let scroll_bar =
+  let _scroll_bar =
     foreign "GuiScrollBar"
       (Types.Rectangle.t @-> int @-> int @-> int @-> returning int)
 
   (*  Grid control *)
-  let grid =
+  let _grid =
     foreign "GuiGrid"
       (Types.Rectangle.t @-> float @-> int @-> returning Types.Vector2.t)
 
   (* Advance controls set *)
   (*  List View control, returns selected list item index *)
-  let list_view =
+  let _list_view =
     foreign "GuiListView"
       (Types.Rectangle.t @-> string @-> ptr int @-> int @-> returning int)
 
   (*  List View with extended parameters *)
-  let list_view_ex =
+  let _list_view_ex =
     foreign "GuiListViewEx"
       (Types.Rectangle.t @-> ptr string @-> int @-> ptr int @-> ptr int @-> int
      @-> returning int)
 
   (*  Message Box control, displays a message *)
-  let message_box =
+  let _message_box =
     foreign "GuiMessageBox"
       (Types.Rectangle.t @-> string @-> string @-> string @-> returning int)
 
   (*  Text Input Box control, ask for text *)
-  let text_input_box =
+  let _text_input_box =
     foreign "GuiTextInputBox"
       (Types.Rectangle.t @-> string @-> string @-> string @-> string
      @-> returning int)
 
   (*  Color Picker control (multiple color controls) *)
-  let color_picker =
+  let _color_picker =
     foreign "GuiColorPicker"
       (Types.Rectangle.t @-> Types.Color.t @-> returning Types.Color.t)
 
   (*  Color Panel control *)
-  let color_panel =
+  let _color_panel =
     foreign "GuiColorPanel"
       (Types.Rectangle.t @-> Types.Color.t @-> returning Types.Color.t)
 
   (*  Color Bar Alpha control *)
-  let color_bar_alpha =
+  let _color_bar_alpha =
     foreign "GuiColorBarAlpha" (Types.Rectangle.t @-> float @-> returning float)
 
   (*  Color Bar Hue control *)
-  let color_bar_hue =
+  let _color_bar_hue =
     foreign "GuiColorBarHue" (Types.Rectangle.t @-> float @-> returning float)
 
   (* Styles loading functions *)
