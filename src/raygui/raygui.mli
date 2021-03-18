fa9d04df7a917efcbde9991d77b6b04e8fc73b60
Tobias Mock <tobiasjammer@googlemail.com>
2021-03-18 21:59:46 +0100

    Clean up interface

--- src/raygui/raygui.mli
+++ src/raygui/raygui.mli
@@ -1,166 +1,14 @@
-module ControlState = Raygui_generated_constants.ControlState
-module TextAlignment = Raygui_generated_constants.TextAlignment
-module Wrapped_Control = Raygui_generated_constants.Wrapped_Control
-module ControlProperty = Raygui_generated_constants.ControlProperty
-module DefaultProperty = Raygui_generated_constants.DefaultProperty
-module ToggleProperty = Raygui_generated_constants.ToggleProperty
-module SliderProperty = Raygui_generated_constants.SliderProperty
-module ProgressBarProperty = Raygui_generated_constants.ProgressBarProperty
-module CheckBoxProperty = Raygui_generated_constants.CheckBoxProperty
-module ComboBoxProperty = Raygui_generated_constants.ComboBoxProperty
-module DropdownBoxProperty = Raygui_generated_constants.DropdownBoxProperty
-module TextBoxProperty = Raygui_generated_constants.TextBoxProperty
-module SpinnerProperty = Raygui_generated_constants.SpinnerProperty
-module ScrollBarProperty = Raygui_generated_constants.ScrollBarProperty
-module ScrollBarSide = Raygui_generated_constants.ScrollBarSide
-module ListViewProperty = Raygui_generated_constants.ListViewProperty
-module ColorPickerProperty = Raygui_generated_constants.ColorPickerProperty
-module StyleProp = Raygui_generated_constants.StyleProp
+module ControlState : sig
+  type t = Normal | Focused | Pressed | Disabled
 
-val enable : unit -> unit
+  val to_int : t -> int
+end
 
-val disable : unit -> unit
+module TextAlignment : sig
+  type t = Left | Center | Right
 
-val lock : unit -> unit
-
-val unlock : unit -> unit
-
-val fade : float -> unit
-
-val set_state : ControlState.t -> unit
-
-val get_state : unit -> ControlState.t
-
-val set_font : Raylib.Font.t -> unit
-
-val get_font : unit -> Raylib.Font.t
-
-val _set_style :
-  Raygui_functions.Constants.Wrapped_Control.t -> int -> int -> unit
-
-val _get_style : Raygui_functions.Constants.Wrapped_Control.t -> int -> int
-
-val enable_tooltip : unit -> unit
-
-val disable_tooltip : unit -> unit
-
-val set_tooltip : string -> unit
-
-val clear_tooltip : unit -> unit
-
-val window_box : Raylib.Rectangle.t -> string -> bool
-
-val group_box : Raylib.Rectangle.t -> string -> unit
-
-val line : Raylib.Rectangle.t -> string -> unit
-
-val panel : Raylib.Rectangle.t -> unit
-
-val scroll_panel :
-  Raylib.Rectangle.t ->
-  Raylib.Rectangle.t ->
-  Raylib.Vector2.t Raylib.ptr ->
-  Raylib.Rectangle.t
-
-val label : Raylib.Rectangle.t -> string -> unit
-
-val button : Raylib.Rectangle.t -> string -> bool
-
-val label_button : Raylib.Rectangle.t -> string -> bool
-
-val image_button : Raylib.Rectangle.t -> string -> Raylib.Texture.t -> bool
-
-val image_button_ex :
-  Raylib.Rectangle.t -> string -> Raylib.Texture.t -> Raylib.Rectangle.t -> bool
-
-val toggle : Raylib.Rectangle.t -> string -> bool -> bool
-
-val toggle_group : Raylib.Rectangle.t -> string -> int -> int
-
-val check_box : Raylib.Rectangle.t -> string -> bool -> bool
-
-val combo_box : Raylib.Rectangle.t -> string -> int -> int
-
-val dropdown_box : Raylib.Rectangle.t -> string -> int -> bool -> int * bool
-
-val spinner :
-  Raylib.Rectangle.t ->
-  string ->
-  int ->
-  min:int ->
-  max:int ->
-  bool ->
-  int * bool
-
-val value_box :
-  Raylib.Rectangle.t ->
-  string ->
-  int ->
-  min:int ->
-  max:int ->
-  bool ->
-  int * bool
-
-val text_box : Raylib.Rectangle.t -> string -> bool -> string * bool
-
-val text_box_multi : Raylib.Rectangle.t -> string -> bool -> string * bool
-
-val slider :
-  Raylib.Rectangle.t ->
-  string ->
-  string ->
-  float ->
-  min:float ->
-  max:float ->
-  float
-
-val slider_bar :
-  Raylib.Rectangle.t ->
-  string ->
-  string ->
-  float ->
-  min:float ->
-  max:float ->
-  float
-
-val progress_bar :
-  Raylib.Rectangle.t ->
-  string ->
-  string ->
-  float ->
-  min:float ->
-  max:float ->
-  float
-
-val status_bar : Raylib.Rectangle.t -> string -> unit
-
-val dummy_rec : Raylib.Rectangle.t -> string -> unit
-
-val scroll_bar : Raylib.Rectangle.t -> int -> int -> int -> int
-
-val grid : Raylib.Rectangle.t -> float -> int -> Raylib.Vector2.t
-
-val list_view : Raylib.Rectangle.t -> string -> int -> int -> int * int
-
-val list_view_ex :
-  Raylib.Rectangle.t -> string list -> int -> int -> int -> int * int * int
-
-val message_box : Raylib.Rectangle.t -> string -> string -> string -> int
-
-val text_input_box :
-  Raylib.Rectangle.t -> string -> string -> string -> string -> string * int
-
-val color_picker : Raylib.Rectangle.t -> Raylib.Color.t -> Raylib.Color.t
-
-val color_panel : Raylib.Rectangle.t -> Raylib.Color.t -> Raylib.Color.t
-
-val color_bar_alpha : Raylib.Rectangle.t -> float -> float
-
-val color_bar_hue : Raylib.Rectangle.t -> float -> float
-
-val load_style : string -> unit
-
-val load_style_default : unit -> unit
+  val to_int : t -> int
+end
 
 module Default : sig
   type t = [ `Background_color | `Line_color | `Text_size | `Text_spacing ]
@@ -266,6 +114,212 @@ module Control : sig
     | StatusBar of prop
 end
 
-val get_style : Control.t -> int
+(** {1 State modification functions } *)
+
+val enable : unit -> unit
+(** [enable ()] Enable gui controls (global state) *)
+
+val disable : unit -> unit
+(** [disable ()] Disable gui controls (global state) *)
+
+val lock : unit -> unit
+(** [lock ()] Lock gui controls (global state) *)
+
+val unlock : unit -> unit
+(** [unlock ()] Unlock gui controls (global state) *)
+
+val fade : float -> unit
+(** [fade alpha] Set gui controls alpha (global state), alpha goes from 0.0f to 1.0f *)
+
+val set_state : ControlState.t -> unit
+(** [set_state state] Set gui state (global state) *)
+
+val get_state : unit -> ControlState.t
+(** [get_state ()] Get gui state (global state) *)
+
+(** {1 Font set/get functions } *)
+
+val set_font : Raylib.Font.t -> unit
+(** [set_font font] Set gui custom font (global state) *)
+
+val get_font : unit -> Raylib.Font.t
+(** [get_font ()] Get gui custom font (global state) *)
+
+(** {1 Style set/get functions } *)
 
 val set_style : Control.t -> int -> unit
+(** [set_style control value] Set one style property *)
+
+val get_style : Control.t -> int
+(** [get_style control] Get one style property *)
+
+(** {1 Tooltips set functions } *)
+
+val enable_tooltip : unit -> unit
+(** [enable_tooltip ()] Enable gui tooltips *)
+
+val disable_tooltip : unit -> unit
+(** [disable_tooltip ()] Disable gui tooltips *)
+
+val set_tooltip : string -> unit
+(** [set_tooltip tooltip] Set current tooltip for display *)
+
+val clear_tooltip : unit -> unit
+(** [clear_tooltip ()] Clear any tooltip registered *)
+
+(** {1 Container/separator controls, useful for controls organization } *)
+
+val window_box : Raylib.Rectangle.t -> string -> bool
+(** [window_box bounds title] Window Box control, shows a window that can be closed *)
+
+val group_box : Raylib.Rectangle.t -> string -> unit
+(** [group_box bounds text] Group Box control with text name *)
+
+val line : Raylib.Rectangle.t -> string -> unit
+(** [line bounds text] Line separator control, could contain text *)
+
+val panel : Raylib.Rectangle.t -> unit
+(** [panel bounds] Panel control, useful to group controls *)
+
+val scroll_panel :
+  Raylib.Rectangle.t ->
+  Raylib.Rectangle.t ->
+  Raylib.Vector2.t Raylib.ptr ->
+  Raylib.Rectangle.t
+(** [scroll_panel bounds content scroll] Scroll Panel control *)
+
+(** {1 Basic controls set } *)
+
+val label : Raylib.Rectangle.t -> string -> unit
+(** [label bounds text] Label control, shows text *)
+
+val button : Raylib.Rectangle.t -> string -> bool
+(** [button bounds text] Button control, returns true when clicked *)
+
+val label_button : Raylib.Rectangle.t -> string -> bool
+(** [label_button bounds text] Label button control, show true when clicked *)
+
+val image_button : Raylib.Rectangle.t -> string -> Raylib.Texture.t -> bool
+(** [image_button bounds text texture] Image button control, returns true when clicked *)
+
+val image_button_ex :
+  Raylib.Rectangle.t -> string -> Raylib.Texture.t -> Raylib.Rectangle.t -> bool
+(** [image_button_ex bounds text texture tex_source] Image button extended control, returns true when clicked *)
+
+val toggle : Raylib.Rectangle.t -> string -> bool -> bool
+(** [toggle bounds text active] Toggle Button control, returns true when active *)
+
+val toggle_group : Raylib.Rectangle.t -> string -> int -> int
+(** [toggle_group bounds text active] Toggle Group control, returns active toggle index *)
+
+val check_box : Raylib.Rectangle.t -> string -> bool -> bool
+(** [check_box bounds text checked] Check Box control, returns true when active *)
+
+val combo_box : Raylib.Rectangle.t -> string -> int -> int
+(** [combo_box bounds text active] Combo Box control, returns selected item index *)
+
+val dropdown_box : Raylib.Rectangle.t -> string -> int -> bool -> int * bool
+(** [dropdown_box bounds text active] Dropdown Box control, returns selected item, active *)
+
+val spinner :
+  Raylib.Rectangle.t ->
+  string ->
+  int ->
+  min:int ->
+  max:int ->
+  bool ->
+  int * bool
+(** [spinner bounds text valute ~min ~max edit_mode] Spinner control, returns selected value, edit_mode *)
+
+val value_box :
+  Raylib.Rectangle.t ->
+  string ->
+  int ->
+  min:int ->
+  max:int ->
+  bool ->
+  int * bool
+(** [value_box bounds text value ~min ~max edit_mode] Value Box control, updates input text with numbers, returns value, edit_mode *)
+
+val text_box : Raylib.Rectangle.t -> string -> bool -> string * bool
+(** [text_box bounds text text_size edit_mode] Text Box control, returns text, edit_mode *)
+
+val text_box_multi : Raylib.Rectangle.t -> string -> bool -> string * bool
+(** [text_box_multi bounds text text_size edit_mode] Text Box control with multiple lines,returns text, edit_mode *)
+
+val slider :
+  Raylib.Rectangle.t ->
+  string ->
+  string ->
+  float ->
+  min:float ->
+  max:float ->
+  float
+(** [slider bounds text_left text_right value ~min ~max] Slider control, returns selected value *)
+
+val slider_bar :
+  Raylib.Rectangle.t ->
+  string ->
+  string ->
+  float ->
+  min:float ->
+  max:float ->
+  float
+(** [slider_bar bounds text_left text_right value ~min ~max] Slider Bar control, returns selected value *)
+
+val progress_bar :
+  Raylib.Rectangle.t ->
+  string ->
+  string ->
+  float ->
+  min:float ->
+  max:float ->
+  float
+(** [progress_bar bounds text_left text_right value ~min ~max] Progress Bar control, shows current progress value *)
+
+val status_bar : Raylib.Rectangle.t -> string -> unit
+(** [status_bar bounds text] Status Bar control, shows info text *)
+
+val dummy_rec : Raylib.Rectangle.t -> string -> unit
+(** [dummy_rec bounds text] Dummy control for placeholders *)
+
+val scroll_bar : Raylib.Rectangle.t -> int -> min:int -> max:int -> int
+(** [scroll_bar bounds value ~min ~max] Scroll Bar control *)
+
+val grid : Raylib.Rectangle.t -> float -> int -> Raylib.Vector2.t
+(** [grid bounds spacing subdivs] Grid control *)
+
+(** {1 Advance controls set } *)
+
+val list_view : Raylib.Rectangle.t -> string -> int -> int -> int * int
+(** [list_view bounds text scross_index active] List View control, returns selected list item index, active *)
+
+val list_view_ex :
+  Raylib.Rectangle.t -> string list -> int -> int -> int -> int * int * int
+(** [list_view_ex bounds text_list focus scroll_index active] List View with extended parameters, returns selected list item index, focus, active  *)
+
+val message_box : Raylib.Rectangle.t -> string -> string -> string -> int
+(** [message_box bounds title message buttons] Message Box control, displays a message *)
+
+val text_input_box :
+  Raylib.Rectangle.t -> string -> string -> string -> string -> string * int
+(** [text_input_box bounds title message buttons text] Text Input Box control, returns text, state *)
+
+val color_picker : Raylib.Rectangle.t -> Raylib.Color.t -> Raylib.Color.t
+(** [color_picker bounds color] Color Picker control (multiple color controls) *)
+
+val color_panel : Raylib.Rectangle.t -> Raylib.Color.t -> Raylib.Color.t
+(** [color_panel bounds color] Color Panel control *)
+
+val color_bar_alpha : Raylib.Rectangle.t -> float -> float
+(** [color_bar_alpha bounds alpha] Color Bar Alpha control *)
+
+val color_bar_hue : Raylib.Rectangle.t -> float -> float
+(** [color_bar_hue bounds value] Color Bar Hue control *)
+
+(** Styles loading functions *)
+val load_style : string -> unit
+(** [load_style filename] Load style file (.rgs) *)
+
+val load_style_default : unit -> unit
+(** [load_style_default ()] Load style default over global style *)
