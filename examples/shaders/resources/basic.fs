ae19eaa85b9d5007486b1a3c89d3651476d6695a
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2021-12-13 21:24:18 +0100

    Add instanced rendering example

---
+++ examples/shaders/resources/basic.fs
@@ -0,0 +1,15 @@
+#version 330
+
+// Input vertex attributes (from vertex shader)
+in vec3 fragPosition;
+in vec2 fragTexCoord;
+in vec4 fragColor;
+in vec3 fragNormal;
+
+// Output fragment color
+out vec4 finalColor;
+
+void main()
+{
+    finalColor = vec4(fragPosition, 1.0);
+}
