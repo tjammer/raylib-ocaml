55063ff66f5a0c284992b80e5f3887497d78b075
Tobias Mock <mail@tmock.de>
2024-09-23 23:11:48 +0200

    Fix build for older OCaml versions
    wip

--- src/c/apply_patch.ml
+++ src/c/apply_patch.ml
@@ -2,28 +2,32 @@
    goes wrong with opening the file or patching, we want the execution to fail.
    There is no straying from the happy path. *)
 
-open In_channel
-
 let patch file =
-  with_open_bin file (fun ic ->
-      let s = really_input_string ic (in_channel_length ic) |> Option.get in
+  let patch_ic = open_in_bin file in
+  let s = really_input_string patch_ic (in_channel_length patch_ic) in
+  close_in patch_ic;
 
-      let patch = Patch.parse s |> List.hd in
-      let target =
-        match patch.operation with
-        | Edit (a, _) -> String.trim a
-        | _ -> failwith "We only do edits"
-      in
-      print_endline target;
-      with_open_bin target
-        Out_channel.(
-          fun ic ->
-            really_input_string ic (in_channel_length ic) |> fun input ->
-            print_endline ("patching " ^ target);
+  let patch = Patch.parse s |> List.hd in
+  let target =
+    match patch.operation with
+    | Edit (a, _) -> String.trim a
+    | _ -> failwith "We only do edits"
+  in
+  print_endline target;
+  let input_ic = open_in_bin target in
+  let input = really_input_string input_ic (in_channel_length input_ic) in
+  close_in input_ic;
 
-            Patch.patch input patch |> Option.get |> fun output ->
-            with_open_bin target (fun oc ->
-                output_string oc output;
-                flush oc)))
+  print_endline ("patching " ^ target);
+
+  let output =
+    match Patch.patch (Some input) patch with
+    | Some output -> output
+    | None -> failwith "Patch does not apply"
+  in
+  let oc = open_out_bin target in
+  output_string oc output;
+  flush oc;
+  close_out oc
 
 let () = patch (Array.get Sys.argv 1)
