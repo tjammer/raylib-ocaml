aac0d0793b552035153f77338a73bf0a58d8f029
Tobias Mock <mail@tmock.de>
2024-12-18 23:02:10 +0100

    Compile raygui implementation

--- src/c/raygui/functions.ml
+++ src/c/raygui/functions.ml
@@ -2,7 +2,6 @@ module Functions (F : Ctypes.FOREIGN) = struct
   open Ctypes
   open F
   module Constants = Fixed_types
-  module Types = Raylib_fixed_types
 
   (* State modification functions *)
   (*  Enable gui controls (global state) *)
@@ -30,10 +29,10 @@ module Functions (F : Ctypes.FOREIGN) = struct
 
   (* Font set/get functions *)
   (*  Set gui custom font (global state) *)
-  let _set_font = foreign "GuiSetFont" (Types.Font.t @-> returning void)
+  let _set_font = foreign "GuiSetFont" (Raylib.Font.t @-> returning void)
 
   (*  Get gui custom font (global state) *)
-  let _get_font = foreign "GuiGetFont" (void @-> returning Types.Font.t)
+  let _get_font = foreign "GuiGetFont" (void @-> returning Raylib.Font.t)
 
   (* Style set/get functions *)
   (*  Set one style property *)
@@ -48,160 +47,160 @@ module Functions (F : Ctypes.FOREIGN) = struct
   (* Container/separator controls, useful for controls organization *)
   (*  Window Box control, shows a window that can be closed *)
   let _window_box =
-    foreign "GuiWindowBox" (Types.Rectangle.t @-> string @-> returning bool)
+    foreign "GuiWindowBox" (Raylib.Rectangle.t @-> string @-> returning bool)
 
   (*  Group Box control with text name *)
   let _group_box =
-    foreign "GuiGroupBox" (Types.Rectangle.t @-> string @-> returning void)
+    foreign "GuiGroupBox" (Raylib.Rectangle.t @-> string @-> returning void)
 
   (*  Line separator control, could contain text *)
-  let _line = foreign "GuiLine" (Types.Rectangle.t @-> string @-> returning void)
+  let _line = foreign "GuiLine" (Raylib.Rectangle.t @-> string @-> returning void)
 
   (*  Panel control, useful to group controls *)
-  let _panel = foreign "GuiPanel" (Types.Rectangle.t @-> returning void)
+  let _panel = foreign "GuiPanel" (Raylib.Rectangle.t @-> returning void)
 
   (*  Scroll Panel control *)
   let _scroll_panel =
     foreign "GuiScrollPanel"
-      (Types.Rectangle.t @-> Types.Rectangle.t @-> ptr Types.Vector2.t
-      @-> returning Types.Rectangle.t)
+      (Raylib.Rectangle.t @-> Raylib.Rectangle.t @-> ptr Raylib.Vector2.t
+      @-> returning Raylib.Rectangle.t)
 
   (* Basic controls set *)
   (*  Label control, shows text *)
   let _label =
-    foreign "GuiLabel" (Types.Rectangle.t @-> string @-> returning void)
+    foreign "GuiLabel" (Raylib.Rectangle.t @-> string @-> returning void)
 
   (*  Button control, returns true when clicked *)
   let _button =
-    foreign "GuiButton" (Types.Rectangle.t @-> string @-> returning bool)
+    foreign "GuiButton" (Raylib.Rectangle.t @-> string @-> returning bool)
 
   (*  Label button control, show true when clicked *)
   let _label_button =
-    foreign "GuiLabelButton" (Types.Rectangle.t @-> string @-> returning bool)
+    foreign "GuiLabelButton" (Raylib.Rectangle.t @-> string @-> returning bool)
 
   (*  Toggle Button control, returns true when active *)
   let _toggle =
     foreign "GuiToggle"
-      (Types.Rectangle.t @-> string @-> bool @-> returning bool)
+      (Raylib.Rectangle.t @-> string @-> bool @-> returning bool)
 
   (*  Toggle Group control, returns active toggle index *)
   let _toggle_group =
     foreign "GuiToggleGroup"
-      (Types.Rectangle.t @-> string @-> int @-> returning int)
+      (Raylib.Rectangle.t @-> string @-> int @-> returning int)
 
   (*  Check Box control, returns true when active *)
   let _check_box =
     foreign "GuiCheckBox"
-      (Types.Rectangle.t @-> string @-> bool @-> returning bool)
+      (Raylib.Rectangle.t @-> string @-> bool @-> returning bool)
 
   (*  Combo Box control, returns selected item index *)
   let _combo_box =
     foreign "GuiComboBox"
-      (Types.Rectangle.t @-> string @-> int @-> returning int)
+      (Raylib.Rectangle.t @-> string @-> int @-> returning int)
 
   (*  Dropdown Box control, returns selected item *)
   let _dropdown_box =
     foreign "GuiDropdownBox"
-      (Types.Rectangle.t @-> string @-> ptr int @-> bool @-> returning bool)
+      (Raylib.Rectangle.t @-> string @-> ptr int @-> bool @-> returning bool)
 
   (*  Spinner control, returns selected value *)
   let _spinner =
     foreign "GuiSpinner"
-      (Types.Rectangle.t @-> string @-> ptr int @-> int @-> int @-> bool
+      (Raylib.Rectangle.t @-> string @-> ptr int @-> int @-> int @-> bool
      @-> returning bool)
 
   (*  Value Box control, updates input text with numbers *)
   let _value_box =
     foreign "GuiValueBox"
-      (Types.Rectangle.t @-> string @-> ptr int @-> int @-> int @-> bool
+      (Raylib.Rectangle.t @-> string @-> ptr int @-> int @-> int @-> bool
      @-> returning bool)
 
   (*  Text Box control, updates input text *)
   let _text_box =
     foreign "GuiTextBox"
-      (Types.Rectangle.t @-> ptr char @-> int @-> bool @-> returning bool)
+      (Raylib.Rectangle.t @-> ptr char @-> int @-> bool @-> returning bool)
 
   (*  Text Box control with multiple lines *)
   let _text_box_multi =
     foreign "GuiTextBoxMulti"
-      (Types.Rectangle.t @-> ptr char @-> int @-> bool @-> returning bool)
+      (Raylib.Rectangle.t @-> ptr char @-> int @-> bool @-> returning bool)
 
   (*  Slider control, returns selected value *)
   let _slider =
     foreign "GuiSlider"
-      (Types.Rectangle.t @-> string @-> string @-> float @-> float @-> float
+      (Raylib.Rectangle.t @-> string @-> string @-> float @-> float @-> float
      @-> returning float)
 
   (*  Slider Bar control, returns selected value *)
   let _slider_bar =
     foreign "GuiSliderBar"
-      (Types.Rectangle.t @-> string @-> string @-> float @-> float @-> float
+      (Raylib.Rectangle.t @-> string @-> string @-> float @-> float @-> float
      @-> returning float)
 
   (*  Progress Bar control, shows current progress value *)
   let _progress_bar =
     foreign "GuiProgressBar"
-      (Types.Rectangle.t @-> string @-> string @-> float @-> float @-> float
+      (Raylib.Rectangle.t @-> string @-> string @-> float @-> float @-> float
      @-> returning float)
 
   (*  Status Bar control, shows info text *)
   let _status_bar =
-    foreign "GuiStatusBar" (Types.Rectangle.t @-> string @-> returning void)
+    foreign "GuiStatusBar" (Raylib.Rectangle.t @-> string @-> returning void)
 
   (*  Dummy control for placeholders *)
   let _dummy_rec =
-    foreign "GuiDummyRec" (Types.Rectangle.t @-> string @-> returning void)
+    foreign "GuiDummyRec" (Raylib.Rectangle.t @-> string @-> returning void)
 
   (*  Scroll Bar control *)
   let _scroll_bar =
     foreign "GuiScrollBar"
-      (Types.Rectangle.t @-> int @-> int @-> int @-> returning int)
+      (Raylib.Rectangle.t @-> int @-> int @-> int @-> returning int)
 
   (*  Grid control *)
   let _grid =
     foreign "GuiGrid"
-      (Types.Rectangle.t @-> float @-> int @-> returning Types.Vector2.t)
+      (Raylib.Rectangle.t @-> float @-> int @-> returning Raylib.Vector2.t)
 
   (* Advance controls set *)
   (*  List View control, returns selected list item index *)
   let _list_view =
     foreign "GuiListView"
-      (Types.Rectangle.t @-> string @-> ptr int @-> int @-> returning int)
+      (Raylib.Rectangle.t @-> string @-> ptr int @-> int @-> returning int)
 
   (*  List View with extended parameters *)
   let _list_view_ex =
     foreign "GuiListViewEx"
-      (Types.Rectangle.t @-> ptr string @-> int @-> ptr int @-> ptr int @-> int
+      (Raylib.Rectangle.t @-> ptr string @-> int @-> ptr int @-> ptr int @-> int
      @-> returning int)
 
   (*  Message Box control, displays a message *)
   let _message_box =
     foreign "GuiMessageBox"
-      (Types.Rectangle.t @-> string @-> string @-> string @-> returning int)
+      (Raylib.Rectangle.t @-> string @-> string @-> string @-> returning int)
 
   (*  Text Input Box control, ask for text *)
   let _text_input_box =
     foreign "GuiTextInputBox"
-      (Types.Rectangle.t @-> string @-> string @-> string @-> ptr char
+      (Raylib.Rectangle.t @-> string @-> string @-> string @-> ptr char
      @-> returning int)
 
   (*  Color Picker control (multiple color controls) *)
   let _color_picker =
     foreign "GuiColorPicker"
-      (Types.Rectangle.t @-> Types.Color.t @-> returning Types.Color.t)
+      (Raylib.Rectangle.t @-> Raylib.Color.t @-> returning Raylib.Color.t)
 
   (*  Color Panel control *)
   let _color_panel =
     foreign "GuiColorPanel"
-      (Types.Rectangle.t @-> Types.Color.t @-> returning Types.Color.t)
+      (Raylib.Rectangle.t @-> Raylib.Color.t @-> returning Raylib.Color.t)
 
   (*  Color Bar Alpha control *)
   let _color_bar_alpha =
-    foreign "GuiColorBarAlpha" (Types.Rectangle.t @-> float @-> returning float)
+    foreign "GuiColorBarAlpha" (Raylib.Rectangle.t @-> float @-> returning float)
 
   (*  Color Bar Hue control *)
   let _color_bar_hue =
-    foreign "GuiColorBarHue" (Types.Rectangle.t @-> float @-> returning float)
+    foreign "GuiColorBarHue" (Raylib.Rectangle.t @-> float @-> returning float)
 
   (* Styles loading functions *)
   (*  Load style file (.rgs) *)
